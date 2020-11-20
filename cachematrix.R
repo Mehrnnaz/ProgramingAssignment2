91dfdd606ff496948470e7a0b88b52bd166ba4b7
Mehrnnaz <mehrnaz.adn@gmail.com>
2020-11-20 21:41:01 +0330

    hello, im just testing

---
+++ cachematrix.R
@@ -0,0 +1,20 @@
+## Sorry I'm just testing, gonna resubmit this later
+## CHANGE
+## PLEASE!!
+## WTF
+
+
+
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) { x
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
